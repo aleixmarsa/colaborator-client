5ead7e7744e231d28e87fd8243c73ccb27d7529a
Aleix Marsà <98183799+aleixmarsa@users.noreply.github.com>
2022-07-20 20:08:39 +0200

    search component created

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -6,6 +6,7 @@ import { MenuAlt1Icon, XIcon } from "@heroicons/react/outline";
 import icon from "../../assets/icon.png";
 import { AuthContext } from "../../context/auth.context";
 import Avatar from "react-avatar";
+import SearchMenu from "../menus/SearchMenu";
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
@@ -41,32 +42,11 @@ const NavBar = (props) => {
                   co[lab]orator
                 </h1>
               </div>
-              {/* Search section */}
-              {location.pathname === "/" ? (
-                <div className="flex-1 flex justify-center">
-                  <div className="w-1/2 px-2 ">
-                    <label htmlFor="search" className="sr-only">
-                      Search projects
-                    </label>
-                    <div className="relative text-lime-200 focus-within:text-gray-400">
-                      <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                        <SearchIcon className="h-5 w-5" aria-hidden="true" />
-                      </div>
-                      <input
-                        id="search"
-                        name="search"
-                        className="block w-full pl-10 pr-3 py-2 border border-transparent rounded-md leading-5 bg-lime-400 bg-opacity-25 text-lime-100 placeholder-lime-200 focus:outline-none focus:bg-white focus:ring-0 focus:placeholder-gray-400 focus:text-gray-900 sm:text-sm"
-                        placeholder="Search projects"
-                        type="search"
-                        value={search}
-                        onChange={handleSearch}
-                      />
-                    </div>
-                  </div>
-                </div>
-              ) : (
-                <></>
-              )}
+              <SearchMenu
+                location={location}
+                search={search}
+                handleSearch={handleSearch}
+              />
               <div className="flex lg:hidden">
                 {/* Mobile menu button */}
                 <Disclosure.Button className="bg-lime-600 inline-flex items-center justify-center p-2 rounded-md text-lime-400 hover:text-white hover:bg-lime-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-lime-600 focus:ring-white">
