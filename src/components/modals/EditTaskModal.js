e4cba2322b713d2d3c00e204326800f1cd2e7f57
Guillem Alerany Perez <guillemfata@gmail.com>
2022-07-25 11:17:04 +0200

    Error solved

--- src/components/modals/EditTaskModal.js
+++ src/components/modals/EditTaskModal.js
@@ -148,7 +148,7 @@ const EditTaskModal = (props) => {
                       </div>
                     </div>
                   </div>
-                </div>
+
                 <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
                   <button
                     type="button"
