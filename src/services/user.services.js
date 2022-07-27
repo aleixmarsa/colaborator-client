f619ad36078b73a365be5e52d81cd55018339909
Aleix Marsà <98183799+aleixmarsa@users.noreply.github.com>
2022-07-27 18:32:02 +0200

    url production env changed

--- src/services/user.services.js
+++ src/services/user.services.js
@@ -5,25 +5,5 @@ const URL = "/colaborator-API/users";
 const getAllUsersService = () => {
   return service.get(`${URL}/`);
 };
-// const getAllCurrentProjectsService = () => {
-//   return service.get(`${URL}/current`);
-// };
-// const getAllCompletedProjectsService = () => {
-//   return service.get(`${URL}/completed`);
-// };
-// const getProjectDetailsService = (id) => {
-//   return service.get(`${URL}/${id}`);
-// };
 
-// const addNewProjectService = (newProject) => {
-//   return service.post(`${URL}/`, newProject);
-// };
-
-// const updateProjectService = (id, updatedProject) => {
-//   return service.put(`${URL}/${id}`,updatedProject);
-// };
-
-// const deleteProjectService = (id) => {
-//   return service.delete(`${URL}/${id}`);
-// };
 export { getAllUsersService };
