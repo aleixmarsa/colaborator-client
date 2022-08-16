48388a140e9faaec167fbda2d9cdb739a6c2cbe6
aleixmarsa <98183799+aleixmarsa@users.noreply.github.com>
2022-08-16 23:45:08 +0200

    Revert "fix room not created when getting all projects. Create, update and delete project information is sended via direct socket event"
    This reverts commit 2a82e9bbaabd4c2b0453252380bc77bf5e49aa72.

--- src/pages/ProjectsPage.js
+++ src/pages/ProjectsPage.js
@@ -66,7 +66,7 @@ const ProjectsPage = () => {
       setEditProjectForm(false);
       setNewProjectForm(false);
       socket.emit("getCurrentProjects");
-      // socket.emit("joinProjectRoom", project._id.toString());
+      socket.emit("joinProjectRoom", project._id.toString());
     });
 
     socket.on("getCurrentProjects", (allCurrentProjects) => {
@@ -82,18 +82,18 @@ const ProjectsPage = () => {
       setNewProjectForm(false);
       socket.emit("getCurrentProjects");
       socket.emit("leaveProjectRoom", projectRoom)
-      // updatedProject.team.forEach((member) => {
-      //   if (member._id === user._id) {
-      //     console.log("USER: ", member.name, "IS A MEMBER");
-      //     socket.emit("joinProjectRoom", projectRoom);
-      //   }
-      // });
+      updatedProject.team.forEach((member) => {
+        if (member._id === user._id) {
+          console.log("USER: ", member.name, "IS A MEMBER");
+          socket.emit("joinProjectRoom", projectRoom);
+        }
+      });
     });
 
-    socket.on("projectDeleted", (deletedProject)=>{
-      console.log("🚀 ~ file: ProjectsPage.js ~ line 94 ~ socket.on ~ projectId", deletedProject)
+    socket.on("projectDeleted", (projectId)=>{
+      console.log("🚀 ~ file: ProjectsPage.js ~ line 94 ~ socket.on ~ projectId", projectId)
       setModalHasRender(false);
-      socket.emit("leaveProjectRoom", deletedProject._id.toString())
+      socket.emit("leaveProjectRoom", projectId)
       socket.emit("getCurrentProjects");
       
     })
@@ -103,7 +103,7 @@ const ProjectsPage = () => {
 
   useEffect(() => {
     socket.emit("getCurrentProjects");
-    // socket.emit("joinAllProjectsRoom");
+    socket.emit("joinAllProjectsRoom");
   }, []);
 
   // const socketConnection = () => {
