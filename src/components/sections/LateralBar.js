879bd6c3c2fa6a679fa0a75a332c94d1f40d9ede
aleixmarsa <98183799+aleixmarsa@users.noreply.github.com>
2022-08-17 19:29:07 +0200

    feat show/hide button for the lateral bar

--- src/components/sections/LateralBar.js
+++ src/components/sections/LateralBar.js
@@ -4,19 +4,21 @@ import {
   ArrowLeftIcon,
   TagIcon,
   HomeIcon,
+  BookOpenIcon,
 } from "@heroicons/react/outline";
 
 import { useLocation } from "react-router-dom";
 import { NavLink } from "react-router-dom";
+import { useState } from "react";
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
 
 function LateralBar(props) {
-
   const { projectId } = props;
   const { pathname } = useLocation();
+  const [open, setOpen] = useState(false);
 
   const navigation = [
     {
@@ -53,7 +55,7 @@ function LateralBar(props) {
     },
     {
       name: "Projects",
-      icon: ArrowLeftIcon,
+      icon: BookOpenIcon,
       href: "#",
       current: false,
       keyLink: "",
@@ -62,21 +64,15 @@ function LateralBar(props) {
   ];
 
   return (
-    <div className=" hidden xl:flex lg:flex flex-col flex-grow h-screen border-r border-gray-200 pb-4 bg-neutral-50">
-      <div className="mt-1 flex-grow flex flex-col xl:mr-16">
+    <div className={`flex flex-col  h-screen border-r border-gray-200 pb-4 bg-neutral-50 relative ${open ? "w-48" : "w-24"} duration-300`} >
+      <ArrowLeftIcon
+        className={`w-6 h-6 text-white bg-mainColor absolute -right-3 top-6 rounded-full border-1 border-mainColor cursor-pointer ${!open && "rotate-180"} duration-300`}
+        onClick={() => setOpen(!open)}
+      />
+      <div className="mt-1 flex-grow flex flex-col ">
         <nav className="flex-1 space-y-1" aria-label="Sidebar">
           {navigation.map((item) => (
-            <div key={item.name}className="flex flex-row ml-2 mt-2">
-              <item.icon
-                className={classNames(
-                  item.current
-                    ? "text-gray-500"
-                    : "text-mainColor group-hover:text-gray-500",
-                  "mt-2 mr-3 flex-shrink-0 h-6 w-6"
-                )}
-                aria-hidden="true"
-              />
-
+            <div key={item.name} className="flex flex-row ml-2 mt-2">
               <NavLink
                 to={`${item.link}`}
                 className={() =>
@@ -85,10 +81,25 @@ function LateralBar(props) {
                     : "flex flex-col px-3 py-2 mx-1 text-md font-medium text-mainColor bg-neutral-50 hover:text-buttonHover"
                 }
               >
+                <item.icon
+                  className="hover:text-buttonHover text-left
+                   flex-shrink-0 h-6 w-6"
+                  aria-hidden="true"
+                />
+              </NavLink>
+             <NavLink
+                to={`${item.link}`}
+                className={() =>
+                  [`${item.link}`].includes(pathname)
+                    ? `flex flex-col pr-3 py-2 mx-1 text-md font-medium text-buttonHover bg-neutral-50 hover:text-buttonHover ${!open && "hidden"} duration-300`
+                    : `flex  flex-col pr-3 py-2 mx-1 text-md font-medium text-mainColor bg-neutral-50 hover:text-buttonHover ${!open && "hidden"} duration-300`
+                }
+              >
                 <span className="hover:text-buttonHover text-left">
                   {item.name}
                 </span>
               </NavLink>
+              
             </div>
           ))}
         </nav>
