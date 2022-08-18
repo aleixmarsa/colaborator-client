c86613bf3f5b46352cdd4c5f63b76d6c2882e1d8
Aleix Marsà <98183799+aleixmarsa@users.noreply.github.com>
2022-08-18 17:14:19 +0200

    feat formik login

--- src/components/forms/LogInForm.js
+++ src/components/forms/LogInForm.js
@@ -1,121 +1,223 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
+
+import {useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "./../../context/auth.context";
+import { loginService } from "../../services/auth.services";
+import { Formik } from "formik";
+import * as Yup from "yup";
 import Button from "../buttons/Button";
 import { Link } from "react-router-dom";
+import { ErrorMessage } from "formik";
+import { ExclamationCircleIcon } from "@heroicons/react/solid";
 
-const LogInForm = (props) => {
-  const { handleLoginSubmit, email, handleEmail, password, handlePassword } =
-    props;
+const LogInForm = () => {
+  //   const { handleLoginSubmit, email, handleEmail, password, handlePassword } =
+  //     props;
 
-    return (
+//   const [email, setEmail] = useState("admin@admin.com");
+//   const [password, setPassword] = useState("Admin123!");
+//   const [errorMessage, setErrorMessage] = useState(undefined);
+  const navigate = useNavigate();
+  const { logInUser } = useContext(AuthContext);
+//   const handleEmail = (e) => setEmail(e.target.value);
+//   const handlePassword = (e) => setPassword(e.target.value);
+
+  function classNames(...classes) {
+    return classes.filter(Boolean).join(" ");
+  }
+
+  return (<Formik
+    initialValues={{ email: "", password: "" }}
+    onSubmit={async (values) => {
+      const email = values.email;
+      const password = values.password;
+      const requestBody = { email, password };
+
+      try {
+        const response = await loginService(requestBody);
+        const token = response.data.authToken;
+        logInUser(token);
+        navigate("/projects");
+      } catch (err) {
+        // const errorDescription = err?.response?.data?.message;
+        // setErrorMessage(errorDescription);
+      }
+    }}
+    validationSchema={Yup.object().shape({
+      email: Yup.string().email().required("Required"),
+      password: Yup.string()
+        .required("No password provided.")
+        .min(6, "Password is too short - should be 6 chars minimum.")
+        .matches(
+          /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])/,
+          "Password must contain at least one number, one lowercase and one uppercase letter."
+        ),
+    })}
+  >
+    {props => {
+      const {
+        values,
+        touched,
+        errors,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+      } = props;
+
+      return (
         <div className="flex-grow flex flex-col justify-center py-12 sm:px-6 lg:px-8">
-            <div className="sm:mx-auto sm:w-full sm:max-w-md">
-                <h2 className="mt-6 text-center text-3xl font-bold text-gray-900">
-                Log in to your account
-                </h2>
-                <p className="mt-2 text-center text-sm text-gray-600">
-                Or try it using{" "}
-                <span className="font-bold"> admin@admin.com//Admin123!</span>
-                </p>
-            </div>
+          <div className="sm:mx-auto sm:w-full sm:max-w-md">
+            <h2 className="mt-6 text-center text-3xl font-bold text-gray-900">
+              Log in to your account
+            </h2>
+            <p className="mt-2 text-center text-sm text-gray-600">
+              Or try it using{" "}
+              <span className="font-bold"> admin@admin.com//Admin123!</span>
+            </p>
+          </div>
 
-            <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-                <div className="bg-white py-8 px-4 drop-shadow-xl sm:rounded-md sm:px-10">
-                    <form
-                        onSubmit={handleLoginSubmit}
-                        className="space-y-6"
-                        action="#"
-                        method="POST"
-                    >
-                        <div>
-                            <label
-                                htmlFor="email"
-                                className="block text-sm font-medium text-gray-700"
-                            >
-                                Email address
-                            </label>
-                            <div className="mt-1">
-                                <input
-                                id="email"
-                                name="email"
-                                type="email"
-                                value={email}
-                                autoComplete="email"
-                                onChange={handleEmail}
-                                required
-                                className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm"
-                                />
-                            </div>
-                        </div>
-
-                        <div>
-                            <label
-                                htmlFor="password"
-                                className="block text-sm font-medium text-gray-700"
-                            >
-                                Password
-                            </label>
-                            <div className="mt-1">
-                                <input
-                                id="password"
-                                name="password"
-                                type="password"
-                                value={password}
-                                autoComplete="current-password"
-                                onChange={handlePassword}
-                                required
-                                className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm"
-                                />
-                            </div>
-                        </div>
-
-                        <div className="flex items-center justify-between">
-                            <div className="flex items-center">
-                                <input
-                                id="remember-me"
-                                name="remember-me"
-                                type="checkbox"
-                                className="h-4 w-4 text-green-600 focus:ring-green-500 border-gray-300 rounded"
-                                />
-                                <label
-                                htmlFor="remember-me"
-                                className="ml-2 block text-sm text-gray-900"
-                                >
-                                Remember me
-                                </label>
-                            </div>
-
-                            <div className="text-sm">
-                                <a
-                                href="#"
-                                className="font-bold text-gray-600 hover:text-gray-500 underline"
-                                >
-                                Forgot your password?
-                                </a>
-                            </div>
-                        </div>
-
-                        <div className="flex flex-col">
-                            <Button
-                                position="column"
-                                type="submit"
-                                text="Log In"
-                                color="mainColor"
-                            />
-
-                            <Link to="/signup">
-                                <Button
-                                position="column"
-                                type="submit"
-                                text="Sign Up"
-                                color="white"
-                                />
-                            </Link>
-                        </div>
-                    </form>
+          <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+            <div className="bg-white py-8 px-4 drop-shadow-xl sm:rounded-md sm:px-10">
+              <form
+                onSubmit={handleSubmit}
+                className="space-y-6"
+                action="#"
+                method="POST"
+              >
+                <div>
+                  <label
+                    htmlFor="email"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Email address
+                  </label>
+                  <div className="mt-1 relative">
+                    <input
+                      id="email"
+                      name="email"
+                      type="email"
+                      value={values.email}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      autoComplete="email"
+                      required
+                      className={classNames(
+                        errors.email
+                          ? "focus:outline-red-500"
+                          : "focus:outline-buttonHover",
+                        "appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline sm:text-sm"
+                      )}
+                    />
+                    {errors.email ? (
+                      <div className="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
+                        <ExclamationCircleIcon
+                          className="h-5 w-5 text-red-500"
+                          aria-hidden="true"
+                        />
+                      </div>
+                    ) : (
+                      <p></p>
+                    )}
+                  </div>
+                  <ErrorMessage
+                    component="div"
+                    name="email"
+                    className="mt-2 text-sm text-red-600"
+                  />
                 </div>
+
+                <div>
+                  <label
+                    htmlFor="password"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Password
+                  </label>
+                  <div className="mt-1 relative">
+                    <input
+                      id="password"
+                      name="password"
+                      type="password"
+                      value={values.password}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      autoComplete="current-password"
+                      required
+                      className={classNames(
+                        errors.password
+                          ? "focus:outline-red-500"
+                          : "focus:outline-buttonHover",
+                        "appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline sm:text-sm"
+                      )}
+                    />
+                    {errors.password ? (
+                      <div className="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
+                        <ExclamationCircleIcon
+                          className="h-5 w-5 text-red-500"
+                          aria-hidden="true"
+                        />
+                      </div>
+                    ) : (
+                      <p></p>
+                    )}
+                  </div>
+                  <ErrorMessage
+                    component="div"
+                    name="password"
+                    className="mt-2 text-sm text-red-600"
+                  />
+                </div>
+
+                {/* <div className="flex items-center justify-between">
+              <div className="flex items-center">
+                <input
+                  id="remember-me"
+                  name="remember-me"
+                  type="checkbox"
+                  className="h-4 w-4 text-green-600 focus:ring-green-500 border-gray-300 rounded"
+                />
+                <label
+                  htmlFor="remember-me"
+                  className="ml-2 block text-sm text-gray-900"
+                >
+                  Remember me
+                </label>
+              </div>
+
+              <div className="text-sm">
+                <a
+                  href="#"
+                  className="font-bold text-gray-600 hover:text-gray-500 underline"
+                >
+                  Forgot your password?
+                </a>
+              </div>
+            </div> */}
+
+                <div className="flex flex-col">
+                  <Button
+                    position="column"
+                    type="submit"
+                    text="Log In"
+                    color="mainColor"
+                  />
+
+                  <Link to="/signup">
+                    <Button
+                      position="column"
+                      type="submit"
+                      text="Sign Up"
+                      color="white"
+                    />
+                  </Link>
+                </div>
+              </form>
             </div>
+          </div>
         </div>
-    );
+      );
+    }}
+  </Formik>)
 };
 
 export default LogInForm;
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,50 +1,21 @@
-import { useState, useContext } from "react";
-import { useNavigate } from "react-router-dom";
-import { AuthContext } from "./../context/auth.context";
-import { loginService } from "../services/auth.services";
-
 import LogInForm from "../components/forms/LogInForm";
 import NavBar from "../components/navbar/NavBar";
 import Footer from "../components/footer/Footer";
 
+
 function LoginPage(props, location) {
-
-  const [email, setEmail] = useState("admin@admin.com");
-  const [password, setPassword] = useState("Admin123!");
-  const [errorMessage, setErrorMessage] = useState(undefined);
-  const navigate = useNavigate();
-  const { logInUser } = useContext(AuthContext);
-  const handleEmail = (e) => setEmail(e.target.value);
-  const handlePassword = (e) => setPassword(e.target.value);
-
-  const handleLoginSubmit = async (e) => {
-    e.preventDefault();
-    const requestBody = { email, password };
-
-    try {
-      const response = await loginService(requestBody);
-
-      const token = response.data.authToken;
-      logInUser(token);
-      navigate("/projects");
-    } catch (err) {
-      const errorDescription = err?.response?.data?.message;
-      setErrorMessage(errorDescription);
-    }
-  };
-
   return (
-    
     <div className="flex bg-neutral-50 flex-col h-screen mb-auto">
       <NavBar />
-      <LogInForm
+
+      <LogInForm />
+      {/* <LogInForm
         handleLoginSubmit={handleLoginSubmit}
         email={email}
         handleEmail={handleEmail}
         password={password}
         handlePassword={handlePassword}
-      />
-			{errorMessage && <p className="error-message">{errorMessage}</p>}
+      /> */}
       <Footer />
     </div>
   );
